47a941a7998943a0f8882fe154ee980ba513769c
victor <chrollolucilfer1402@gmail.com>
2023-11-14 22:00:40 +0530

    fixes issue #1, done with google auth and getting the jwt token from server and storing on localstorage for now

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Meet from './pages/Meet.tsx'
-import Index from './pages/Index.tsx'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Meet from "./pages/Meet.tsx";
+import Index from "./pages/Index.tsx";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Index/>
+    path: "/",
+    element: <Index />,
   },
   {
-    path:"/meet",
-    element:<Meet/>
-  }
-]) 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    path: "/auth/google",
+    element: <Index />,
+  },
+  {
+    path: "/meet",
+    element: <Meet />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
--- src/pages/Meet.tsx
+++ src/pages/Meet.tsx
@@ -1,40 +1,29 @@
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-import MessagingBoard from "../components/MessagingBoard"
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import MessagingBoard from "../components/MessagingBoard";
 
-function Meet(){
-    return (<div>
-        
-        <section className="px-[60px]">
-        <Navbar />    
-            <div className="container  flex gap-[1.88rem] justify-between ">
-            <div className="flex flex-col gap-[1.88rem]">
-
-                <div className="w-[55.1875rem] h-[25.125rem]  bg-off-white">
-
-                </div>
-                <div className=" flex gap-[2.31rem]">
-
-                <div className="w-[12.0625rem] h-[6.4375rem] bg-off-white">
-
-                </div>
-                <div className="w-[12.0625rem] h-[6.4375rem] bg-off-white">
-
-                </div>
-                <div className="w-[12.0625rem] h-[6.4375rem] bg-off-white">
-
-                </div>
-                <div className="w-[12.0625rem] h-[6.4375rem] bg-off-white">
-
-                </div>
-                </div>
+function Meet() {
+  return (
+    <div className="bg-white">
+      <section className="px-[60px]">
+        <Navbar />
+        <div className="container  flex gap-[1.88rem] justify-between ">
+          <div className="flex flex-col gap-[1.88rem]">
+            <div className="w-[55.1875rem] h-[25.125rem]  bg-off-white"></div>
+            <div className=" flex gap-[2.31rem]">
+              <div className="w-[12.0625rem] h-[6.4375rem] bg-off-white"></div>
+              <div className="w-[12.0625rem] h-[6.4375rem] bg-off-white"></div>
+              <div className="w-[12.0625rem] h-[6.4375rem] bg-off-white"></div>
+              <div className="w-[12.0625rem] h-[6.4375rem] bg-off-white"></div>
             </div>
-            <MessagingBoard/>
-            </div>
-        </section>
-        <section>
-            <Footer/>
-        </section>
-    </div>)
+          </div>
+          <MessagingBoard />
+        </div>
+      </section>
+      <section>
+        <Footer />
+      </section>
+    </div>
+  );
 }
-export default Meet
+export default Meet;
