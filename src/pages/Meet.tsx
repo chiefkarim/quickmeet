53537ab0f226231b0046518631c69d793a022bbe
chiefkarim <krimotop2016@gmail.com>
2023-11-19 09:33:36 +0100

    fix 2 camera not working on mobile

--- src/pages/Meet.tsx
+++ src/pages/Meet.tsx
@@ -18,17 +18,17 @@ setScreenWidth(window.innerWidth)
 // clean up media tracks and event listener
 return () => {
  window.removeEventListener('resize',updateScreenWidth)
- const tracks = stream?.getTracks()
- tracks && tracks.forEach((track)=> track.stop())
+ 
 }
 },[screenWidth])
-
 function disableCam(){
   setCamState(false)
 }
 //Get user media
 
 async function  requestMediaAccess(){
+  if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+  console.log("Let's get this party started")
   try{
    const mediaStream = await navigator.mediaDevices.getUserMedia({audio:true,video:{ facingMode:"user",width:screenWidth || 883,height:{min:402}}})
   setStream(mediaStream)
@@ -38,6 +38,9 @@ async function  requestMediaAccess(){
  } catch(error){
   console.error(error)
  }
+}else{
+  console.log("device not supported")
+}
 
 }
 
@@ -50,7 +53,7 @@ async function  requestMediaAccess(){
           <div className="flex flex-col gap-[1rem]">
           
             <div className="localVideo max-w-[65vw] overflow-hidden  min-h-[75vh] relative bg-off-white">
-              {camState == true ? (<video  ref={videoRef}  playsInline={true}  className=" h-full w-full" controls></video>) :
+              {camState == true ? (<video  ref={videoRef}  playsInline={true} muted={true} className=" h-full w-full" controls></video>) :
                 ''}
             <button className="absolute bottom-[10px] right-[30px] z-10 " onClick={disableCam}>disable </button>
             <button className="absolute bottom-[10px] left-[10px] z-10 " onClick={requestMediaAccess}>Allow </button>
