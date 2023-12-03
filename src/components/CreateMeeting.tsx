9a04f07cbeb7a7ab7cd573c70b0cd1f38c996005
chiefkarim <krimotop2016@gmail.com>
2023-12-03 16:09:59 +0100

    fix url in creatMeeting component

--- src/components/CreateMeeting.tsx
+++ src/components/CreateMeeting.tsx
@@ -1,7 +1,8 @@
 
 async function  CreateMeeting (){
-    
-        const response =await fetch("http://localhost:3000/meet/create",{method:'POST'})
+    const url = import.meta.env.VITE_BACKEND_URL;
+
+        const response =await fetch(url,{method:'POST'})
         const data = await response.json()
         if(data.roomID){
             localStorage.setItem("roomDetails",JSON.stringify(data))
