d9c83832c0793193e37d4d5f591a7bb36f05dd14
victor <chrollolucilfer1402@gmail.com>
2024-01-13 22:31:18 +0530

    refactored SocketConnect to fix guest users data, plus few minor changes in createMeeting

--- src/components/SocketConnect.ts
+++ src/components/SocketConnect.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { Socket } from "socket.io-client";
 import { useParams } from "react-router-dom";
@@ -7,43 +7,68 @@ import { SetRoom } from "../redux/roomReducer";
 import { room } from "../redux/roomReducer";
 import { joinStatus } from "../pages/Meet";
 
-export default function SocketConnect(
+type guestUser = {
+  guest_id: string;
+  username: string;
+  userType: string;
+};
+
+export default async function SocketConnect(
   setSocket: (socket: Socket | null) => void,
   setError: (error: string) => void,
   setJoinStatus: (joinStatus: joinStatus) => void
 ) {
   const URL = import.meta.env.VITE_BACKEND_URL;
   const params = useParams();
-  const googleToken = localStorage.getItem("google-token");
-  const guestUser = JSON.parse(
-    localStorage.getItem("guestInformation") || "null"
-  );
+  const [guestUser, setGuestUser] = useState<guestUser>(() => {
+    const guest = JSON.parse(
+      localStorage.getItem("guestInformation") || "null"
+    );
+    if (guest) return guest;
+  });
 
+  const googleToken = localStorage.getItem("google-token");
   const roomDetails: room = useAppSelector((state) => state.room);
   const dispatch = useAppDispatch();
 
   const roomID = params.id;
   let userType: string, userID: string, username: string;
+
+  const createGuest = async () => {
+    const response = await fetch(`${URL}/user/createGuest`);
+    const data = await response.json();
+    data["userType"] = "guest";
+    // console.log({ guest: data });
+    localStorage.setItem("guestInformation", JSON.stringify(data));
+    setGuestUser({ ...data });
+  };
+
   if (googleToken) {
     const user = JSON.parse(localStorage.getItem("userInformation") || "null");
     userType = "registered";
     userID = user.user_id;
     username = user.username;
-  } else {
-    (userType = "guest"),
-      (username = guestUser.username),
-      (userID = guestUser.userID);
+  } else if (guestUser) {
+    userType = "guest";
+    username = guestUser.username;
+    userID = guestUser.guest_id;
   }
+
+  useEffect(() => {
+    if (!googleToken && !guestUser) {
+      createGuest();
+    }
+  }, []);
 
   useEffect(() => {
     const s = io(URL);
     s.on("connect", () => {
       setSocket(s);
 
-      if (roomDetails.roomID === null) {
+      if (roomDetails.roomID === null && (googleToken || guestUser)) {
         s.emit("get-room", { roomID, userType, userID, username });
         s.on("get-room", (roomDetails) => {
-          console.log(roomDetails);
+          // console.log(roomDetails);
           dispatch(SetRoom(roomDetails));
         });
       } else if (roomDetails.roomID !== null) {
@@ -77,5 +102,5 @@ export default function SocketConnect(
         return () => s.disconnect();
       }
     });
-  }, [roomDetails]);
+  }, [roomDetails, guestUser]);
 }
